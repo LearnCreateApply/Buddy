4050bee8c29a3ed8b3c7cadec96931be725a093e
LearnCreateApply <shivamingulkar1234@gmail.com>
2025-03-19 15:19:44 +0000

    KK

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
 const express = require('express')
 const app = express()
-const port = 5000
+const port = 3000
 const path = require('path')
-app.use(express.static('public'))
-
 app.get('/',(req,res)=>{
   res.send('Hello World')
 })
-app.post('/login',(req,res)=>{
-  res.sendFile("/workspaces/Buddy/templates/login.html",()=>{})
+app.get('/login',(req,res)=>{
+  res.sendFile(path.join(__dirname,"templates/login.html"),()=>{})
+})
+app.get('/signup',(req,res)=>{
+  res.sendFile(path.join(__dirname,"templates/signup.html"),()=>{})
 })
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
-
 
 
 
