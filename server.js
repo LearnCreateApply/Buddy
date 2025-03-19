b41118545aa5ed5ab722d692977e9a9b9234f71f
LearnCreateApply <shivamingulkar1234@gmail.com>
2025-03-19 04:07:25 +0000

    kk

--- server.js
+++ server.js
@@ -2,16 +2,59 @@ const express = require('express')
 const app = express()
 const port = 3000
 const path = require('path')
-const fsp = require('fs/promises')
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-}).post('/login',(req,res)=>{
-    res.sendFile(path.join(__dirname,'templates/index.html'))
-}).post('/sinup',(req,res)=>{
-    res.sendFile(path.join(__dirname,'templates/index.html'))
+app.use(express.static('public'))
+app.post('/',(req,res)=>{
+  res.send('Hello World')
 })
-
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const fsp = require('fs/promises')
+// app.use(express.json());
+// app.use(express.static('public'))
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// }).post('/login',(req,res)=>{
+//     res.sendFile(path.join(__dirname,'templates/login.html'))
+// }).post('/signup',(req,res)=>{
+//     res.sendFile(path.join(__dirname,'templates/signup.html'))
+// })
